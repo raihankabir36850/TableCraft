1812aa96b0815488ef342a2a9577135658d32662
raihankabir36850 <raihanku320@gmail.com>
2023-08-28 01:35:39 +0600

    basic component with functionalit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Basic } from './Components/Basic/Basic';
+
+function App() {
+  return (
+    <>
+      <h1>hello data table</h1>
+      <Basic />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Title/Title.tsx
@@ -0,0 +1,4 @@
+export const Title = (props: { title: string }) => {
+  const { title } = props;
+  return <h3>{title}</h3>;
+};
