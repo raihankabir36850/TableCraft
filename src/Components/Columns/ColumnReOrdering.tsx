90fe438da8d99ad4231a8d026dfe93009b7e1bba
raihankabir36850 <raihanku320@gmail.com>
2023-09-01 22:00:03 +0600

    need to apply more logic to implemnt the logic

--- src/Components/Columns/ColumnReOrdering.tsx
+++ src/Components/Columns/ColumnReOrdering.tsx
@@ -7,54 +7,57 @@ export const ColumnReOrdering = () => {
   const [columnArray, setColumnArray] = useState(Array(column).fill(''));
   const [fromArray, setFromArray] = useState();
   const [toArray, setToArray] = useState();
-  const events: (EventTarget & HTMLInputElement)[] = [];
 
   const generalFunction = () => {
-    console.log('finished', events, primaryArray);
+    console.log('finished', primaryArray);
 
-    // const from = primaryArray[0];
-    // const to = primaryArray[primaryArray.length - 1];
-    // modifiedArray[from] = to;
-    // modifiedArray[to] = from;
-    // setFromArray(from);
-    // setToArray(to);
-    // setColumnArray(modifiedArray);
-    setPrimaryArray([primaryArray[0], primaryArray[primaryArray.length - 1]]);
+    const firstEl = parseInt(primaryArray[0].value);
+    const firstElValue = parseInt(primaryArray[0].id);
+    const secondEl = parseInt(primaryArray[primaryArray.length - 1].value);
+    const secondElValue = parseInt(primaryArray[primaryArray.length - 1].id);
+
+    if (firstEl < secondEl || (firstEl == firstElValue && secondEl == secondElValue)) {
+      console.log('if');
+      columnArray[parseInt(primaryArray[0].id)] = primaryArray[primaryArray.length - 1].value;
+      columnArray[parseInt(primaryArray[primaryArray.length - 1].id)] = primaryArray[0].value;
+    } else {
+      console.log('else');
+      columnArray[parseInt(primaryArray[0].id)] = primaryArray[0].value;
+      columnArray[parseInt(primaryArray[primaryArray.length - 1].id)] = primaryArray[primaryArray.length - 1].value;
+    }
+
+    console.log('first', columnArray);
+    setColumnArray(columnArray);
   };
 
   const dragEnterHandler = (_e: ChangeEvent<HTMLInputElement>) => {
-    primaryArray.push(_e.target.id);
+    // if (_e.target.dataset.table !== '') {
+    //   primaryArray.push(_e.target.dataset.table);
+    // }
+
+    primaryArray.push({
+      id: _e.target.id,
+      value: _e.target.dataset.table,
+    });
   };
 
   const dragEndHandler = (_e: ChangeEvent<HTMLInputElement>) => {
     // eslint-disable-next-line no-constant-condition
     if (primaryArray.length >= 2 && primaryArray[primaryArray.length - 1] !== primaryArray[0]) {
       generalFunction();
+      setPrimaryArray([]);
     }
   };
 
-  const columnStr = Array(column)
-    .fill('')
-    .map((x, index) => {
-      console.log(primaryArray, 'primaryArray');
-      const similar = primaryArray.length && primaryArray.find((item, _index) => index == parseInt(item));
+  console.log('columnArray', columnArray);
 
-      if (similar) {
-        console.log(similar, 'similar');
-
-        return (
-          <th key={index} id={index} draggable='true' onDragEnter={(e) => dragEnterHandler(e)} onDragEnd={(e) => dragEndHandler(e)}>
-            column : {similar}
-          </th>
-        );
-      } else {
-        return (
-          <th key={index} id={index} draggable='true' onDragEnter={(e) => dragEnterHandler(e)} onDragEnd={(e) => dragEndHandler(e)}>
-            column : {index}
-          </th>
-        );
-      }
-    });
+  const columnStr = columnArray.map((x, index) => {
+    return (
+      <th key={index} id={index} data-table={x !== '' ? x : index} draggable='true' onDragEnter={(e) => dragEnterHandler(e)} onDragEnd={(e) => dragEndHandler(e)}>
+        column : {x !== '' ? x : index}
+      </th>
+    );
+  });
 
   const tablecells = Array(row)
     .fill('')
